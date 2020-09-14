321eb0c97175eef189a3e0d342954dede913bfe2
Víctor Ceballos Espinosa <victcebesp@gmail.com>
2020-09-14 15:43:44 +0100

    Bug fix: Show alphabetic validation only when there is something written

--- src/validation/validations/alphabeticValidation.js
+++ src/validation/validations/alphabeticValidation.js
@@ -1,5 +1,5 @@
 export default class AlphabeticValidation {
-  validate = (answer) => /^[a-zA-Z]+$/.test(answer)
+  validate = (answer) => /^[a-zA-Z]*$/.test(answer)
 
   getExplanation = () => `The answer can only have alphabetic characters.`
 }
